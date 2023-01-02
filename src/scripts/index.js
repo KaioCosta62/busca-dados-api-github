87124b73e6b1be0ef07b3a2f6f0441f5335db869
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2023-01-02 16:27:52 -0300

    adicionando a requisicao ao apertar a tecla enter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,39 @@
 
 
 const btn = document.querySelector('.btn-search')
+const inputName = document.querySelector('.input-search')
+
+inputName.addEventListener('keyup', (e) => {
+    const userName = inputName.value
+    if(e.keyCode === 13){
+        async function user(userName){
+            const response = await fetch(`https://api.github.com/users/${userName}`)
+            return await response.json()
+        }
+        
+        
+        function getUserProfile(userName){
+            user(userName).then(userData => {
+                const htmlProfileData = document.querySelector('.profile-data')
+                const userInfo = `
+                    <img src = "${userData.avatar_url}" alt = "Foto do usuário"/>
+                    <div class = "data">
+                        <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                        <p>${userData.bio ?? 'Não possui uma bio cadastrada 😢'} </p>
+                    </div>
+                `
+        
+                htmlProfileData.innerHTML = userInfo
+            })
+        }
+
+        getUserProfile(userName)
+    }
+})
 
 btn.addEventListener('click', () => {
-    const inputName = document.querySelector('.input-search')
     const userName = inputName.value
-    
+
     async function user(userName){
         const response = await fetch(`https://api.github.com/users/${userName}`)
         return await response.json()
